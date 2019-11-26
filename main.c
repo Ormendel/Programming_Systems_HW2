511bdf52b777e40c6203bffe6ac2768acae03f33
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2019-11-26 13:02:34 +0200

    Back to the original commit
    Not relating to getchar() and how to clean the buffer...

--- main.c
+++ main.c
@@ -31,90 +31,80 @@ int main()
 	{
 		printf("Enter char:\n");
 		Menu();
-		act=getchar();
-		if(getchar()=='\n')
+		scanf("%c",&act);
+		switch(act)
 		{
-			switch(act)
-			{
-				case 'O':
-					printf("Enter amount to put in a new account:\n");
+			case 'O':
+				printf("Enter amount to put in a new account:\n");
+				scanf("%lf",&amount);
+				amount=(double)((int)(amount*100))/100.0;
+				O(amount);
+				printf("\n");
+			break;
+			case 'B':	
+				printf("Enter account number between 901 to 950\n");
+				scanf("%d",&acc_num);
+				B(acc_num);
+				printf("\n");
+			break;
+			case 'D':
+				printf("Enter account number between 901 to 950\n");
+				scanf("%d",&acc_num);
+				check=isValidNum(acc_num);
+				if(check==1)
+				{
+					printf("Enter amount to desposit in bank:\n");
 					scanf("%lf",&amount);
 					amount=(double)((int)(amount*100))/100.0;
-					O(amount);
+					D(acc_num,amount);
 					printf("\n");
-				break;
-				case 'B':	
-					printf("Enter account number between 901 to 950\n");
-					scanf("%d",&acc_num);
-					B(acc_num);
+				}
+				else
+					printf("%d does not exist\n",acc_num);				
+			break;
+			case 'W':
+				printf("Enter account number between 901 to 950\n");
+				scanf("%d",&acc_num);
+				check=isValidNum(acc_num);
+				if(check==1)
+				{
+					printf("Enter amount to withdraw from bank:\n");
+					scanf("%lf",&amount);
+					amount=(double)((int)(amount*100))/100.0;
+					W(acc_num,amount);
 					printf("\n");
-				break;
-				case 'D':
-					printf("Enter account number between 901 to 950\n");
-					scanf("%d",&acc_num);
-					check=isValidNum(acc_num);
-					if(check==1)
-					{
-						printf("Enter amount to desposit in bank:\n");
-						scanf("%lf",&amount);
-						amount=(double)((int)(amount*100))/100.0;
-						D(acc_num,amount);
-						printf("\n");
-					}
-					else
-						printf("%d does not exist\n",acc_num);				
-				break;
-				case 'W':
-					printf("Enter account number between 901 to 950\n");
-					scanf("%d",&acc_num);
-					check=isValidNum(acc_num);
-					if(check==1)
-					{
-						printf("Enter amount to withdraw from bank:\n");
-						scanf("%lf",&amount);
-						amount=(double)((int)(amount*100))/100.0;
-						W(acc_num,amount);
-						printf("\n");
-					}
-					else
-						printf("%d does not exist\n",acc_num);
-				break;
-				case 'C':
-					printf("Enter account number between 901 to 950\n");
-					scanf("%d",&acc_num);
-					C(acc_num);
-					printf("\n");
-				break;
-				case 'I':
-					printf("Enter Interest rate\n");
-					scanf("%d",&interest_rate);
-					I(interest_rate);
-					printf("\n");
-				break;
-				case 'P':
-					printf("Printing all open accounts:\n");
-					P();
-					printf("\n");
-				break;
-				case 'E':
-					printf("Closing all open accounts:\n");
-					E();
-					printf("\n");
-				break;
+				}
+				else
+					printf("%d does not exist\n",acc_num);
+			break;
+			case 'C':
+				printf("Enter account number between 901 to 950\n");
+				scanf("%d",&acc_num);
+				C(acc_num);
+				printf("\n");
+			break;
+			case 'I':
+				printf("Enter Interest rate\n");
+				scanf("%d",&interest_rate);
+				I(interest_rate);
+				printf("\n");
+			break;
+			case 'P':
+				printf("Printing all open accounts:\n");
+				P();
+				printf("\n");
+			break;
+			case 'E':
+				printf("Closing all open accounts:\n");
+				E();
+				printf("\n");
+			break;
 
-				default:
-					 printf("ERR; Please try again!\n");
-				break;
-			}
-			getchar();	
-			printf("\n");
-		}
-		else
-		{
-			printf("You need to enter only one character!\n\n");
-			int c;
-			while((c=getchar())!='\n'&&c!=EOF){}//Cleaning buffer of getchar()
-		}
+			default:
+				 printf("ERR; Please try again!\n");
+			break;
+		}	
+		printf("\n");
 	}
 	while(act!='E');
 	printf("Bye Bye ! :)\n");
